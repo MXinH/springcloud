976abc80b7545cb4f420a50fdb869feae08d7f45
maxinhui <maxinhui@1hu.me>
2021-07-13 18:36:58 +0800

    ss

--- springcloud_10hystrix_dashboard/src/main/java/com/mxh/HystrixDashboardApplication.java
+++ springcloud_10hystrix_dashboard/src/main/java/com/mxh/HystrixDashboardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-//@EnableDiscoveryClient   //只要引入discovery client依赖，该注解无须现实声明 自动注册
+//@EnableDiscoveryClient   //只要引入discovery client依赖，该注解无须现实声明 自动注册自动注册自动注册
 @EnableHystrixDashboard  //注解用来开启当前应用为仪表盘应用
 public class HystrixDashboardApplication {
 
