6520639486bda37d20fe7662b8df402642a5eec6
maxinhui <maxinhui@1hu.me>
2021-07-22 16:49:58 +0800

    end..

--- springcloud_13configclient/src/main/java/com/mxh/controller/DemoController.java
+++ springcloud_13configclient/src/main/java/com/mxh/controller/DemoController.java
@@ -3,10 +3,12 @@ package com.mxh.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope  //作用:用来在不需要重启微服务情况下，将当前scope域中信息刷新为最新配置信息
 public class DemoController {
 
 
